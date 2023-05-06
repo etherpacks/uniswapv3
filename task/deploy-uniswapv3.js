d909318cbebd608c066db796fee1368724ce2a07
kbrav <>
2023-05-06 16:41:58 -0400

    netname

--- task/deploy-uniswapv3.js
+++ task/deploy-uniswapv3.js
@@ -6,17 +6,21 @@ const { task } = require('hardhat/config')
 
 task('deploy-mock-uniswap', 'deploy mock uniswapv3')
 .setAction(async (args, hre) => {
+  const netname = hre.network.name.replace('_fork', '')
   const [ signer ]  = await hre.ethers.getSigners()
   const weth_pack = args.weth_pack
-  const pack = require('../pack/uniswapv3_ethereum.dpack.json')  // reference deployment for mocks
+  const pack = require(`../pack/uniswapv3_${netname}.dpack.json`)  // reference deployment for mocks
   const dapp = await dpack.load(pack, hre.ethers, signer)
-  const factory  = await dapp._types.UniswapV3Factory.deploy()
+  const factory = dapp.uniswapV3Factory;
+
+  //const factory  = await dapp._types.UniswapV3Factory.deploy()
+  console.log("FACTORY", factory);
   const router = await dapp._types.SwapRouter.deploy(factory.address, weth_pack.objects.weth9.address)
   const mockpack = JSON.parse(JSON.stringify(pack))
-  mockpack.network = hre.network.name.replace('_fork', '')
+  mockpack.network = netname
   mockpack.objects.swapRouter.address = router.address
   mockpack.objects.uniswapV3Factory.address = factory.address
-  const mockpath = path.join(__dirname, `../pack/uniswapv3_${mockpack.network}.dpack.json`)
+  const mockpath = path.join(__dirname, `../pack/uniswapv3_${netname}.dpack.json`)
   const mockjson = JSON.stringify(mockpack, null, 2)
   fs.writeFileSync(mockpath, mockjson)
   return mockpack
